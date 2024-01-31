d269c116b188664e4d3599c058cf91fd55b2e3d6
Kushagra <kushagraj81@gmail.com>
2024-01-31 14:47:30 +0530

    basic exception handling

---
+++ src/main/java/com/db/databaseDemo/Exception/ArraySizeException.java
@@ -0,0 +1,10 @@
+package com.db.databaseDemo.Exception;
+
+public class ArraySizeException extends RuntimeException{
+
+    public ArraySizeException(String Msg)
+    {
+        super(Msg);
+    }
+
+}
--- src/main/java/com/db/databaseDemo/restController/JokeController.java
+++ src/main/java/com/db/databaseDemo/restController/JokeController.java
@@ -1,23 +1,47 @@
 package com.db.databaseDemo.restController;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.db.databaseDemo.Exception.ArraySizeException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 @RestController
 @RequestMapping("/joke")
 public class JokeController {
     Random rand = new Random();
-    @GetMapping("/random")
-    public Object getRandomJoke()
+    List<Object> arr= new ArrayList<>();
+
+    public JokeController()
+    {
+        arr.add("Kushagra is a fool 0");
+        arr.add("Kushagra is a fool 1");
+        arr.add("Kushagra is a fool 2");
+        arr.add("Kushagra is a fool 3");
+    }
+    @GetMapping("/random/{jokeid}")
+    public Object getRandomJoke(@PathVariable int jokeid)
     {
         Map<String, Object> object = new HashMap<>();
         object.put("type", "random");
         object.put("Joke", "your sister is a cauliflower" + rand.nextInt(1000));
+        object.put("joke",jokeid);
         return object;
+    }
+    @GetMapping("/list/{jokeid}")
+    public Object getJokeFromList(@PathVariable int jokeid)
+    {
+        if(jokeid >=arr.size())
+        {
+            throw new ArraySizeException("The id given is out array bounds: " + jokeid);
+        }
+        return arr.get(jokeid);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> throwArraySizeException(ArraySizeException exc)
+    {
+        return new ResponseEntity<>(exc.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
